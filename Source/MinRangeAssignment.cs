06bd9d080b46cf946039d468bfa6c35662503b07
Dingo <oferdottan@gmail.com>
2016-11-11 12:30:27 +0200

    Initial release
    Grenade Fix: Rearmed adds a minRange to explosive weapons in RimWorld so that pawns don't harm themselves.
    
    It works on any ranged explosive, even those added by mods.

---
+++ Source/MinRangeAssignment.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using UnityEngine;
+
+namespace GrenadeFixRearmed
+{
+	public class AssignMinRanges : MonoBehaviour
+	{
+		public virtual void Start()
+		{
+			this.enabled = true;
+		}
+
+		public virtual void Update()
+		{
+			if (PlayDataLoader.Loaded)
+			{
+				GrenadeFixer();
+				this.enabled = false;
+			}
+			else new WaitForSeconds(1);
+		}
+
+		internal void GrenadeFixer()
+		{
+			List<ThingDef> projectileWeapons = DefDatabase<ThingDef>.AllDefs.Where(w => w.Verbs.Exists(v => v.projectileDef != null)).ToList();
+			List<ThingDef> explosives = projectileWeapons.FindAll(v => v.Verbs.Exists(x => x.projectileDef.projectile.explosionRadius > 0f));
+
+#if DEBUG
+			Log.Message("GrenadeFixRearmed :: Found " + explosives.Count + " explosives.");
+#endif
+
+			foreach (var thing in explosives)
+			{
+				var thingVerbs = thing.Verbs;
+				foreach (var verb in thingVerbs)
+				{
+					float explosionRadius = verb.projectileDef.projectile.explosionRadius;
+					verb.minRange = explosionRadius + 0.1f;
+#if DEBUG
+					Log.Message("GrenadeFixRearmed :: Set minRange for " + thing.label + " (" + thing.defName + ")");
+#endif
+				}
+			}
+
+			Log.Message("GrenadeFixRearmed :: Defined minRange for " + explosives.Count + " explosives.");
+		}
+	}
+
+	public class ModInitializer : ITab
+	{
+		public ModInitializer()
+		{
+			var gameObject = new GameObject("Grenade Fix: Rearmed");
+			gameObject.AddComponent<AssignMinRanges>();
+		}
+
+		protected override void FillTab()
+		{
+		}
+	}
+}
